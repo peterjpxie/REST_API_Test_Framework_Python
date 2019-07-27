c981264777efa0bc5850d3d5d8e436d2f16a3aaa
Peter Jiping Xie <peter.jp.xie@gmail.com>
2019-07-27 16:44:42 +0800

    update

--- Scripts/code_snippets/flask_mock_simple_service.py
+++ Scripts/code_snippets/flask_mock_simple_service.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/json', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def test_json():
-    return '{"code": 1, "message": "Hello, World!" }'
+    return '{"code": 1, "message": "Hello World!" }'
     
 # Run in HTTP
-app.run(host='127.0.0.1', port='5000')  
+app.run(host='127.0.0.1', port='5000', debug=True)  
