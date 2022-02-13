c8edde95f47af391feed0b6f40b756e75cb98b03
Peter Jiping Xie <peter.jp.xie@gmail.com>
2022-02-13 16:53:59 +0800

    add parse input func

--- Scripts/test_rest_api.py
+++ Scripts/test_rest_api.py
@@ -27,6 +27,7 @@ import os
 import inspect
 import sys
 from dotmap import DotMap
+import re
 
 if sys.version_info < (3,6):
     raise Exception("Requires Python 3.6 or above.")
@@ -196,23 +197,38 @@ def parse_test_input(filename):
     """
     if not os.path.isfile(filename):
         log.error('parse_test_input: Invalid filename: %s' % filename)
-        raise FileNotFoundError
+        raise FileNotFoundError(filename)
     
     with open(filename,'r') as f:
-
         # 1st line: Method url
         # e.g. POST http://httpbin.org/post 
-        1st_line = f.readline()
-        assert len(1st_line.split()) == 2
-        method, url = 1st_line.split()
+        content = f.read()
+        parts = re.split('\s*\n\s*\n',content)
+        parts_len = len(parts)
+
+        # part 1: Method and url
+        assert len(parts[0].split()) == 2
+        method, url = parts[0].split()
         method, url = method.strip(), url.strip()
 
-        # next line is empty
-        2nd_line = f.readline()
+        # part 2: headers
+        if parts_len > 1 and parts[1].strip() != '': 
+            header_lines = re.split('\s*\n',parts[1])
+            header_lines = [ line.strip() for line in header_lines] # strip line spaces
+            # print(header_lines)
+            headers = dict([ re.split(':\s*', line) for line in header_lines])
+            # print(headers)
+        else:
+            headers = None
 
-        # TODO
+        # part 3: body
+        if parts_len > 2 and parts[2].strip() != '': 
+            body = parts[2].strip().strip('\n')
+            # print(body)
+        else:
+            body = None
 
-
+    return method, url, headers, body
 
 
 class TestAPI:
@@ -400,13 +416,7 @@ class TestAPI:
 
 
 if __name__ == '__main__':
-    # self test
-    var = {
-        "name": {
-            "firstname": "Peter",
-            "secondname": "Xie"
-        },
-        "scores": [{"k1": 1},{"k1":"v2"}],
-        "age": 22
-    }
-    print(dict_to_ini(var,"a.ini"))
+    # self test  
+    method, url, headers, body = parse_test_input('../inputs/test_case_01/request_01.txt')
+    print('\n'.join([method, url, str(headers), body]))
+    pass
